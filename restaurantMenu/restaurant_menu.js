caea284a4aed5bae1ea404600ef374210a3d81ef
liormary <liorms3@gmail.com>
2024-05-08 05:44:49 -0400

    restaurant menu

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,16 @@
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak- 25$', 'Pasta- 14$', 'Burger- 12$', 'Salmon- 20$'];
+const dessertMenu = ['Cake- 5$', 'Ice Cream- 3$', 'Pudding- 10$', 'Fruit Salad- 10$'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
